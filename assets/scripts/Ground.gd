482b58897d86fd6b560bb2c91ae45998307040c5
TheCynderace <thatothercynderace@proton.me>
2025-05-20 01:17:08 -0300

    make ground loop forever

---
+++ assets/scripts/Ground.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+
+
+
+# all this does is make the ground move when the player touches the area2d
+# so it gives the illusion that the ground is infinite
+
+
+
+func _on_body_entered(body: Node2D) -> void:
+	var ground = $".."
+	var player = $"../../../player"
+	if body == player:
+		ground.position.x += 256
