57666e1bed956e7641dd0c30fd4774ca4187f685
TheCynderace <thatothercynderace@proton.me>
2025-05-20 00:51:33 -0300

    icon rotation + accurate jump physics

--- assets/scripts/cube.gd
+++ assets/scripts/cube.gd
@@ -5,16 +5,18 @@ extends CharacterBody2D
 @export var jppower = 2.1333
 const spd_mult = 64
 var spd_portal_mult = 1
-const jp_mult = -640
-const grav = 7000
+const jp_mult = -600
+var grav = 6100
+var rot = 0
 
 func _physics_process(delta: float) -> void:
-	velocity.x = spd * spd_mult * spd_portal_mult
 	if not is_on_floor():
 		velocity.y +=  grav * delta
-		
+		rot += 7 * (delta * 60)
 	else:
-		
+		rot = snapped($cube.rotation_degrees, 90)
 		if Input.is_action_pressed("jump"):
 			velocity.y = jppower * jp_mult
+	$cube.rotation_degrees += (rot - $cube.rotation_degrees) / 3
+	velocity.x = spd * spd_mult * spd_portal_mult
 	move_and_slide()
